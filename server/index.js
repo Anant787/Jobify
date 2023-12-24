c45c200f9b3cfe2363cdef2c39e053702f769003
Abhinav Gautam <ABHINAVGAUTAM.210111063@gehu.ac.in>
2023-12-24 14:39:10 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+console.log("PORT: " + process.env.ATLAS_URI);
 // use middlewares
 // Set up CORS
 // app.use(cors({
